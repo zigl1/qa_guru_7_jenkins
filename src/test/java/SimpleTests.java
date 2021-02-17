391a60d8a52eb24939337f1a18038ae615956d0a
zigl1 <ilgiz7725@gmail.com>
2021-02-17 16:19:50 -0500

    some allure steps

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,6 +17,7 @@ public class SimpleTests {
         assertEquals(1, 0);
 
     }
+
     @Test
     void successWithHamcrestTest() {
         assertThat(true, is(true));
@@ -27,17 +29,20 @@ public class SimpleTests {
         assertThat(true, is(false));
 
     }
+
     @Test
     @DisplayName("Some positive test")
     void successWithHamcrestAndStepsTest() {
-        assertThat(true, is(true));
+        step("Assert that true is true", () ->
+                assertThat(true, is(true)));
 
     }
 
     @Test
     @DisplayName("Some negative test")
     void negativeWithHamcrestAndStepstest() {
-        assertThat(true, is(false));
+        step("Assert that true is false", () ->
+                assertThat(true, is(false)));
 
     }
 }
